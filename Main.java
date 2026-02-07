3ac776ea4527f0977705551eee1e8c359c61e6f9
almuttaqintegalofficial <m.zaqimubarok2@gmail.com>
2026-02-07 10:14:32 +0700

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-package Keperluan;
-
-public class Main {
-    
-    public static void main(String[] args) {
-        System.out.println("Assalamu'alaikum & selamat datang di majelis ta'lim al_muttaqin tegal");
-    }
-    
-}
