84dc53b08303624fc927e2cf1c57420f87ae89cf
almuttaqintegalofficial <m.zaqimubarok2@gmail.com>
2026-02-07 10:13:03 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+package Keperluan;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Assalamu'alaikum & selamat datang di majelis ta'lim al_muttaqin tegal");
+    }
+    
+}
